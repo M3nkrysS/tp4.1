93dc555803b8010dea465d9f2bbca58962402a0c
beaudrytinklerl <beaudrytinklerl@educ.college-montreal.qc.ca>
2024-10-18 14:53:50 -0400

    commit

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ introduction à POOP
 """
 import random
 from math import pi
+from dataclasses import dataclass
+
+
+def de_vinght():
+    nbr = random.randint(1, 20)
+    return nbr
 
 
 class StringFoo:
@@ -37,15 +43,16 @@ class Cercle:
 
     def calcul_circonference(self):
         self.circonference = 2 * pi * self.rayon
-        print(f"la circonférence du cercle est de {self.circonference}")
+        print(f"la circonférence du cercle est de {self.circonference}\n")
 
 
 class Hero:
-    def __init__(self, pv, force_atk, force_def, nom_hero):
-        self.pv = random.randint(2, 20)
-        self.force_atk = random.randint(1, 6)
-        self.force_def = random.randint(1, 6)
+    def __init__(self, nom_hero, pv, force_atk, force_def, vivant):
         self.nom_hero = nom_hero
+        self.pv = pv
+        self.force_atk = force_atk
+        self.force_def = force_def
+        self.vivant = vivant
 
     def faire_attaque(self):
         self.attaque = self.force_atk + random.randint(1, 6)
@@ -53,10 +60,26 @@ class Hero:
     def recevoir_dommages(self, qte_dommage):
         self.pv -= qte_dommage - self.force_def
 
-    #dire si le joueur est en vie en une ligne                                      <----Tu est rendu ici
-    def est_vivant(self):
+    def est_vivant(self, vivant):
+        self.vivant = vivant
+        self.vivant = True
+        if self.pv <= 0:
+            self.vivant = False
+        else:
+            pass
+
+    def vie(self):
+        print(self.vivant)
 
 
+@dataclass
+class PersonnageDnD:
+    force: int = de_vinght()
+    dexterite: int = de_vinght()
+    constitution: int = de_vinght()
+    intelligence: int = de_vinght()
+    sagesse: int = de_vinght()
+    charisme: int = de_vinght()
 
 
 ligne = StringFoo()
@@ -70,3 +93,8 @@ r.afficher_infos()
 c = Cercle(6)
 c.calcul_aire()
 c.calcul_circonference()
+
+h = Hero("George", random.randint(2, 20), random.randint(1, 6), random.randint(1, 6), True)
+h.vie()
+p = PersonnageDnD
+print(p.force)
